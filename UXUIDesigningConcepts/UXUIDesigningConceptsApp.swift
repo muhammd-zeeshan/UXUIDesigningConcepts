ef9dd87ce27fe39d6d12648a83fb49aed859f920
muhmmad-zeeshan <rafaq657@gmail.com>
2024-10-31 19:32:16 +0500

    Initial Commit

---
+++ UXUIDesigningConcepts/UXUIDesigningConceptsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UXUIDesigningConceptsApp.swift
+//  UXUIDesigningConcepts
+//
+//  Created by Muhammad Zeeshan on 31/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct UXUIDesigningConceptsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
